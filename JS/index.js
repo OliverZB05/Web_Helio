c1c1779b33ee979b335366e5a7856ae8cd974bf9
OliverZB05 <106617284+OliverZB05@users.noreply.github.com>
2022-09-18 15:44:23 -0300

    Delete index.js

--- JS/index.js
+++
@@ -1,6 +0,0 @@
-const toggleMenuElement = document.getElementById("toggle-menu");
-const dropdownMenuElement = document.getElementById("dropdown");
-
-toggleMenuElement.addEventListener("click", ()=>{
-    dropdownMenuElement.classList.toggle("home__dropdown--show");
-});
